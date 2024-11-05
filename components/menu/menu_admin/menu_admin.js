089f902f90238f84769833704ff2b6acb3fdc1ad
kinokano <kin2014@hotmail.com>
2024-11-05 19:14:58 -0400

    uptadeMenu
    caminhos absolutos em todos os endereços

--- components/menu/menu_admin/menu_admin.js
+++ components/menu/menu_admin/menu_admin.js
@@ -1,5 +1,5 @@
 async function puxarMenu(){ 
-    fetch('../menu_admin/menu_admin.html').then(response => response.text()).then(data =>{document.getElementById('menuAdmin').innerHTML = data})
+    fetch('http://127.0.0.1:5501/components/menu/menu_admin/menu_admin.html').then(response => response.text()).then(data =>{document.getElementById('menuAdmin').innerHTML = data})
 
     
 }
