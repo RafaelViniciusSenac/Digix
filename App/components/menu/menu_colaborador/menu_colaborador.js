f47daf63be828541a9581f38b39789c1fb8bd216
kinokano <kin2014@hotmail.com>
2024-11-18 21:14:40 -0400

    update_menu_usuario_dropdown
    adicionado a flecha ao lado do sino a função de abrir um menu com funções do usuário

--- App/components/menu/menu_colaborador/menu_colaborador.js
+++ App/components/menu/menu_colaborador/menu_colaborador.js
@@ -1,19 +1,21 @@
 async function puxarMenu(){
-    await fetch('http://127.0.0.1:5501/components/menu/menu_colaborador/menu_colaborador.html').then(response => response.text()).then(data =>{document.getElementById('menuColaborador').innerHTML = data})
+    await fetch('http://127.0.0.1:5501/App/components/menu/menu_colaborador/menu_colaborador.html').then(response => response.text()).then(data =>{document.getElementById('menuColaborador').innerHTML = data})
     const flecha = document.getElementById('flecha')
 const flechaCima = document.getElementById('flechaCima')
 const perfilMenu = document.getElementById('perfilMenu')
+const dropMenu = document.getElementById("dropMenu")
 
 flecha.addEventListener('click',()=>{
     flecha.className = "flecha displayOff"
     flechaCima.className = "flechaCima displayOn"
-   
+    dropMenu.className = "dropMenu DisplayOn"
 
 })
 
 flechaCima.addEventListener("click",()=>{
     flechaCima.className = "flechaCima displayOff"
     flecha.className = "flecha displayOn"
+    dropMenu.className = "dropMenu displayOff"
     
 })
 }
